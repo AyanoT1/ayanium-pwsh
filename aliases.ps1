f3c93c32c91b84b6f0044ac25d616c1e672c7db3
Sergio Romero Veliz <128757548+AyanoT1@users.noreply.github.com>
2025-03-27 10:49:21 -0300

    add: alias ls to hide dotfiles

--- aliases.ps1
+++ aliases.ps1
@@ -1,3 +1,8 @@
+function Get-NormalItemsOnly {
+    Get-ChildItem | Where-Object { -not $_.Name.StartsWith('.') }
+}
+
+Set-Alias -Name 'ls' -Value 'Get-NormalItemsOnly'
 Set-Alias -Name 'vim' -Value 'nvim'
-Set-Alias -Name 'll' -Value 'ls'
+Set-Alias -Name 'll' -Value 'Get-ChildItem'
 Set-Alias -Name 'py' -Value 'python'
